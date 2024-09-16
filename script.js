abda635bdb6efac2799eaae53eb8e318f9b5eec5
Vikram G <152708983+student-vG@users.noreply.github.com>
2024-09-17 00:39:03 +0530

    added documents files and img's

---
+++ script.js
@@ -0,0 +1,16 @@
+// var typed = new Typed(".auto-type",{
+//     strings : ["programmer", "Designer", "Youtuber"],
+//     typeSpeed : 150,
+//     backSpeed : 150,
+//     loop : true
+// });     
+
+
+// Scroll to top button functionality
+const scrollTopBtn = document.getElementById('scroll-top-btn');
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+//  nav-bar
