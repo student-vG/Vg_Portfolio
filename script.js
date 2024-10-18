7de8ec105dedadcce090172240af5c2aa3347fe1
Vikram G <152708983+student-vG@users.noreply.github.com>
2024-10-19 01:00:08 +0530

    Update script.js
    added full updated code

--- script.js
+++ script.js
@@ -7,10 +7,33 @@
 
 
 // Scroll to top button functionality
-const scrollTopBtn = document.getElementById('scroll-top-btn');
+// const scrollTopBtn = document.getElementById('scroll-top-btn');
 
-scrollTopBtn.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+// scrollTopBtn.addEventListener('click', () => {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+// });
 
-//  nav-bar
+//! for animation to element smoothly 
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const boxes = document.querySelectorAll('.box-anim ');
+  
+    const observerOptions = {
+      threshold: 0.5, // 50% of the element must be visible
+    };
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // Stop observing once it's visible
+        }
+      });
+    }, observerOptions);
+  
+    boxes.forEach(box => {
+      observer.observe(box);
+    });
+  });
+  
